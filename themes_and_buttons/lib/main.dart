33e5398a62f506005dbb3585ebae257dd2395b2e
nm8011 <nm8011@rit.edu>
2024-09-24 19:22:33 -0400

    flutter clean projects

--- themes_and_buttons/lib/main.dart
+++ themes_and_buttons/lib/main.dart
@@ -7,119 +7,252 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      //3.)Add a scaffoldBackgroundColor to both light and dark themes.
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        //13.) Alter the default font of the theme to Courier.
+        fontFamily: 'Courier',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        //3.)light background to grey[300].
+        scaffoldBackgroundColor: Colors.grey[300],
+        //TextTheme
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 48,
+            fontWeight: FontWeight.w900,
+          ),
+          displayMedium: TextStyle(
+            color: Color.fromARGB(255, 68, 0, 255),
+            fontSize: 44,
+            fontWeight: FontWeight.w800,
+          ),
+          displaySmall: TextStyle(
+            color: Color.fromARGB(255, 80, 0, 146),
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Color.fromARGB(255, 0, 75, 40),
+            fontSize: 36,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyMedium: TextStyle(
+            color: Color.fromARGB(255, 125, 0, 0),
+            fontSize: 32,
+            fontWeight: FontWeight.w500,
+          ),
+          bodySmall: TextStyle(
+            color: Colors.brown,
+            fontSize: 28,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(
+              Colors.black,
+            ),
+            foregroundColor: WidgetStateProperty.all(
+              const Color.fromARGB(255, 80, 0, 146),
+            ),
+            textStyle: WidgetStateProperty.all(
+              const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //2.) Add a darkTheme to the MaterialApp, and
+      darkTheme: ThemeData(
+        //2b.)apply a new color scheme based on a seed color of red.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+        //3.) Set the dark background to blueGrey[500]
+        scaffoldBackgroundColor: Colors.blueGrey[500],
+        //TextTheme
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 48,
+            fontWeight: FontWeight.w900,
+          ),
+          displayMedium: TextStyle(
+            color: Color.fromRGBO(122, 255, 34, 1),
+            fontSize: 44,
+            fontWeight: FontWeight.w800,
+          ),
+          displaySmall: TextStyle(
+            color: Color.fromARGB(255, 242, 255, 0),
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Color.fromARGB(255, 49, 95, 248),
+            fontSize: 36,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyMedium: TextStyle(
+            color: Color.fromARGB(255, 255, 0, 0),
+            fontSize: 32,
+            fontWeight: FontWeight.w500,
+          ),
+          bodySmall: TextStyle(
+            color: Color.fromARGB(255, 246, 78, 255),
+            fontSize: 28,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(
+              const Color.fromRGBO(122, 255, 34, 1),
+            ),
+            foregroundColor: WidgetStateProperty.all(Colors.white),
+            textStyle: WidgetStateProperty.all(
+              const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+      ),
+
+      //1.) Add a themeMode attribute to your MaterialApp and set it to light.
+      themeMode: ThemeMode.light,
+
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("IGME-340: Themes"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            //12.) Create an ElevatedButton with an icon.
+            ElevatedButton.icon(
+              onPressed: () {
+                print("ElevatedButton With Icon Pressed");
+              },
+              icon: const Icon(Icons.add_a_photo),
+              label: const Text(
+                "Add a Photo",
+                style: TextStyle(fontSize: 14),
+              ),
+              style: ButtonStyle(
+                padding: WidgetStateProperty.all(
+                  const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            //10.) Add a, TextButton and OutlinedButton
+            TextButton(
+              onPressed: () {
+                print("TextButton Pressed");
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(255, 28, 138, 92)),
+                foregroundColor:
+                    WidgetStateProperty.all(const Color.fromARGB(255, 0, 0, 0)),
+              ),
+              child: const Text(
+                "Text Button",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
+            OutlinedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(255, 206, 130, 0)),
+                foregroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(255, 200, 255, 0)),
+              ),
+              child: const Text(
+                "Text Button",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 200,
+              //4.) Change the colors of container to material theme colors => green to primary
+              color: Theme.of(context).colorScheme.primary,
+              child: Text(
+                "I am Primary",
+                //Change each container's text to a different textTheme,
+                //observe the changes when the app reloads.
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              //4.) yellow to secondary
+              color: Theme.of(context).colorScheme.secondary,
+              child: Text(
+                "I am Secondary",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            Container(
+              width: 350,
+              height: 100,
+              //4.) pink to error
+              color: Theme.of(context).colorScheme.error,
+              child: Text(
+                "I am Error",
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("ElevatedButton1 Pressed");
+              },
+              child: const Text('Elevated Button'),
+            ),
+            //7.) Add another ElevatedButton
+            ElevatedButton(
+              onPressed: () {
+                print("ElevatedButton2 Pressed");
+              },
+              child: const Text(
+                'Elevated2 Button',
+                style: TextStyle(
+                  fontSize: 26,
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
