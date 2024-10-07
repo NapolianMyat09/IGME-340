18b206dd9ce1cafe42c1ee64f96449fe800e4887
napolianmyat09 <myat.napolian@gmail.com>
2024-10-06 23:48:39 -0400

    finished and clean

--- textfield_dropdown_buttons/lib/main.dart
+++ textfield_dropdown_buttons/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TextField & DropDown Buttons',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Cool Game Beta Signup Form'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,237 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //VARIABLES
+  String displayText = "Your Awesome Personal Data will show here!";
+  // void displayChangedText(String newText) {
+  //   displayText = newText;
+  // }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String email_ = '';
+  String name_ = '';
+  String phoneNumber_ = '';
+  String dob_ = '';
+  List<String> listOfItems = <String>[
+    "In-Person",
+    "Email",
+    "Voice Call",
+    "Text Message"
+  ];
+  String contactPref_ = "In-Person";
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
+
+  void submitForm() {
+    displayText =
+        "Name: $name_\nEmail: $email_\nDOB: $dob_\nPhone: $phoneNumber_\nContact Pref $contactPref_";
   }
 
+  void changeEmail(TextEditingController tec) {
+    email_ = tec.text;
+  }
+
+  ///style custom text
+  Text myText(String textString, double fontsize_,
+      [fontWeight_ = FontWeight.normal,
+      fontColor_ = Colors.black,
+      textHeight_ = 1.2]) {
+    return Text(textString,
+        style: TextStyle(
+            fontSize: fontsize_,
+            fontWeight: fontWeight_,
+            color: fontColor_,
+            height: textHeight_));
+  }
+
+  SizedBox inputTextField(IconData icon_, String text_, TextInputType inputType,
+      TextEditingController inputController, Function(String) onChanged_) {
+    return SizedBox(
+      child: TextField(
+          keyboardType: inputType,
+          controller: inputController,
+          obscureText: false,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(18)),
+              labelText: text_,
+              icon: Icon(icon_,
+                  size: 40, color: Colors.grey), //Add icon before textfield
+              //X icon to clear inputted values
+              suffixIcon: IconButton(
+                icon: Icon(Icons.clear),
+                onPressed: () => {
+                  setState(() {
+                    inputController.text = '';
+                    onChanged_(
+                        ''); //without this, when clearing, there exists previous set value which is never cleared
+                  }),
+                }, //clears the input
+              )),
+          onChanged:
+              onChanged_), //calls function which changes the string text ie. Email, to user inputs
+    );
+  }
+
+  SizedBox dropDownButton() {
+    return SizedBox(
+        width: 400,
+        child: DropdownButton<String>(
+          menuWidth: 400,
+          isExpanded: true, //sends dropdown carrot to the foremost right
+          value: contactPref_,
+          underline: Container(), //hide bottom shadow
+          items: listOfItems.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child:
+                  Text(value, style: TextStyle(fontWeight: FontWeight.normal)),
+            );
+          }).toList(),
+          onChanged: (value) {
+            setState(() {
+              contactPref_ = value!;
+            });
+          },
+        ));
+  }
+
+  ////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        //blue background
+        backgroundColor: Color(0xff3f51b6),
+        title: myText(widget.title, 20, FontWeight.w600, Colors.white),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15, 0, 15, 0), //outside padding
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Welcome
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: myText("Welcome to your Doom!", 28, FontWeight.bold),
+                ),
+                //Text Output
+                Container(
+                  height: 200,
+                  width: 400,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.black, width: 3), //add border
+                      borderRadius: BorderRadius.circular(10)), //round edge
+                  padding: EdgeInsets.all(10), //for text
+                  child: myText(displayText, 22),
+                ),
+                //Text instruction
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: myText(
+                      "Enter your information to get the latest news on our awesome game!",
+                      20),
+                ),
+                //Information Column
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    inputTextField(Icons.person_rounded, "Name",
+                        TextInputType.name, nameController, (value) {
+                      setState(() {
+                        name_ = value;
+                      });
+                    }),
+                    inputTextField(Icons.email, "Email",
+                        TextInputType.emailAddress, emailController, (value) {
+                      setState(() {
+                        email_ = value;
+                      });
+                    }),
+                    inputTextField(Icons.date_range, "Date of Birth",
+                        TextInputType.datetime, dobController, (value) {
+                      setState(() {
+                        dob_ = value;
+                      });
+                    }),
+                    inputTextField(Icons.phone, "Phone", TextInputType.phone,
+                        phoneNumberController, (value) {
+                      setState(() {
+                        phoneNumber_ = value;
+                      });
+                    }),
+                    Row(children: [
+                      Icon(
+                        Icons.contact_support,
+                        size: 55,
+                        color: Colors.grey,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          padding: EdgeInsets.fromLTRB(10, 05, 10, 0),
+                          child: dropDownButton(),
+                        ),
+                      )
+                    ]),
+                  ],
+                ),
+
+                //Reset&Submit Button
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                            backgroundColor: Colors.deepOrange,
+                          ),
+                          icon: Icon(Icons.clear, color: Colors.white),
+                          onPressed: () {
+                            setState(() {
+                              displayText = "";
+                            });
+                          },
+                          label: Text("Reset Form",
+                              style: TextStyle(color: Colors.white))),
+                      ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                            backgroundColor: Color(0xff3f51b6),
+                          ),
+                          icon: Icon(Icons.check, color: Colors.white),
+                          onPressed: () {
+                            setState(() {
+                              submitForm();
+                            });
+                          },
+                          label: Text("Submit Form",
+                              style: TextStyle(color: Colors.white)))
+                    ],
+                  ),
+                ),
+
+                //
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
