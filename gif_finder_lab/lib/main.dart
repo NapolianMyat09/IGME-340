e6c222c6e20f53529cd42acee63eb9d181637954
napolianmyat09 <myat.napolian@gmail.com>
2024-10-19 21:19:29 -0400

    semi finished

--- gif_finder_lab/lib/main.dart
+++ gif_finder_lab/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Giphy Finder Lab',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Giphy Finder'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,211 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Color standardBlue = Color(0xFF1e96f4);
+  List<int> numList = <int>[
+    10,
+    20,
+    30,
+    40,
+    50,
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  ///TextStyle
+  Text myText(String textString, double fontsize_,
+      [fontWeight_ = FontWeight.normal,
+      fontColor_ = Colors.white,
+      textHeight_ = 1.2]) {
+    return Text(
+      textString,
+      style: TextStyle(
+          fontSize: fontsize_,
+          fontWeight: fontWeight_,
+          color: fontColor_,
+          height: textHeight_),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Color(0xFF1e96f4),
+        title: Row(children: [
+          //GIF ICON
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
+            ),
+            child: Center(
+              child: myText(
+                "GIF",
+                16,
+                FontWeight.w900,
+                const Color(0xFF1e96f4),
+              ),
+            ),
+          ),
+          const SizedBox(width: 25),
+          //TITLE TEXT
+          myText(widget.title, 24, FontWeight.w600),
+        ]),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(15),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            //USER INPUT SELECTION
+            //SEARCH TERMS
+            TextField(
+              keyboardType: TextInputType.name,
+              obscureText: false,
+              decoration: InputDecoration(
+                labelText: "Search Term",
+                hintText: "What do you want to find?",
+                hintStyle: const TextStyle(color: Colors.grey),
+                labelStyle: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 3,
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 10), //spacing
+            //NUMBER OF RESULTS
+            DropdownButtonFormField<int>(
+              value: numList[0],
+              decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3.0), // Set the border color and thickness here
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 3.0), // Set the border color and thickness here
+                ),
+              ),
+              items: numList.map<DropdownMenuItem<int>>((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text("$value",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      )),
+                );
+              }).toList(),
+              onChanged: (value) {
+                // Handle dropdown value change
+              },
             ),
+
+            //BUTTONS
+            SizedBox(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 87,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          backgroundColor: standardBlue),
+                      child: myText("Reset", 15, FontWeight.w500)),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: 155,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          backgroundColor: standardBlue),
+                      child: myText("Find some Gifs!", 15, FontWeight.w500)),
+                )
+              ],
+            )),
+
+            ///GIF FOUND!
+            Expanded(
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(5),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[100],
+                    child: const Text("He'd have you all unravel at the"),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[200],
+                    child: const Text('Heed not the rabble'),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[300],
+                    child: const Text('Sound of screams but the'),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[400],
+                    child: const Text('Who scream'),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[500],
+                    child: const Text('Revolution is coming...'),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.teal[600],
+                    child: const Text('Revolution, they...'),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
